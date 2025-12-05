05b552d68f23001f9df8c4b094f25ccb16772511
Rebecka Kinn <rebeckaki@getacademy.no>
2025-12-05 13:06:01 +0100

    added some base html, and header structure

---
+++ components/header.js
@@ -0,0 +1,26 @@
+
+function header(){
+    document.getElementById('header').innerHTML = /*HTML*/`
+    <section>
+        <div>
+            <img src="" alt="TechLabs logo" loading="lazy">
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="../index.html">products</a>
+                </li>
+                <li>
+                    <a href="../cart.html">cart
+                    <img src="" alt="" loading="lazy">
+                    </a> 
+                </li>
+                <li>
+                    <a href="../account/login.html">log in</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+    `;
+}
