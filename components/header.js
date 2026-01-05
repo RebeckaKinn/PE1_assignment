088254da0842b0af6b56b1580f55704607d83375
Rebecka Kinn <rebeckaki@getacademy.no>
2026-01-05 11:22:59 +0100

    making the header mobile friendly according to the design

--- components/header.js
+++ components/header.js
@@ -10,21 +10,30 @@ function header(){
             </picture>
         </div>
 
-            <ul class="menu">
-                <li>
-                    <a href="../index.html">products</a>
-                </li>
-                <li>
-                    <a href="../cart.html">cart
-                    <div>
-                        <img src="public/icons/cart.svg" alt="" loading="lazy">
-                    </div>
-                    </a> 
-                </li>
-                <li>
-                    <a class="button red" href="../account/login.html">log in</a>
-                </li>
-            </ul>
+        <nav id="navigation">
+            <input class="" id="menu" type="checkbox" aria-label="Toggle navigation menu" aria-expanded="false" aria-controls="menu-list"/>
+            <label for="menu" class="hidden-on-desktop" aria-label="Open menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+
+                <ul class="menu">
+                    <li>
+                        <a href="../index.html">products</a>
+                    </li>
+                    <li>
+                        <a href="../cart.html">cart
+                        <div>
+                            <img src="public/icons/cart.svg" alt="" loading="lazy">
+                        </div>
+                        </a> 
+                    </li>
+                    <li>
+                        <a class="button red" href="../account/login.html">log in</a>
+                    </li>
+                </ul>
+            </nav>
     </section>
     `;
 }
