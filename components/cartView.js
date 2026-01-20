3691e1de9dd5f63f4a462025caa684cda762ce09
Rebecka Kinn <rebeckaki@getacademy.no>
2026-01-20 11:50:01 +0100

    fixing css on cart

--- components/cartView.js
+++ components/cartView.js
@@ -36,20 +36,22 @@ function showCartItems(cartItems){
     return cartItems
     .map(
       (item) => /*HTML*/`
-        <div class="cart-item shadow">
-          <img src="${item.image.url}" alt="${item.image.alt}"/>
-          <div>
-            <h5>${item.title}</h5>
-                <div>
-                    <p>price:</p>
-                    <p class="h5">${item.discountedPrice || item.price} kr</p>
+        <div class="cart-item">
+            <div class="item-image">
+                <img src="${item.image.url}" alt="${item.image.alt}"/>
+            </div>
+          <div class="cart-information">
+            <h5 class="remove-default">${item.title}</h5>
+                <div class="txt-space-evenly">
+                    <p class="remove-default">price:</p>
+                    <p class="remove-default highlight">${item.discountedPrice || item.price} kr</p>
                 </div>
-                <div>
-                    <p>qty:</p>
-                    <div>
-                        <button onclick="decreaseQuantity('${item.id}')">-</button>
-                        <p>${item.quantity}</p>
-                        <button onclick="increaseQuantity('${item.id}')">+</button>
+                <div class="txt-space-evenly">
+                    <p class="remove-default">qty:</p>
+                    <div class="button-container">
+                        <button class="round-button" onclick="decreaseQuantity('${item.id}')">-</button>
+                        <p class="remove-default highlight">${item.quantity}</p>
+                        <button class="round-button" onclick="increaseQuantity('${item.id}')">+</button>
                     </div>
                 </div>
           </div>
